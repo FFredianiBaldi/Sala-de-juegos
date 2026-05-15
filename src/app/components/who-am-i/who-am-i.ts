c23769e7b12a5946da65c866aac8dce9e5957987
Federico <fedfredbal@gmail.com>
2026-05-15 03:23:20 -0300

    navbar basico y navegacion entre componentes

---
+++ src/app/components/who-am-i/who-am-i.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-who-am-i',
+  imports: [],
+  templateUrl: './who-am-i.html',
+  styleUrl: './who-am-i.css',
+})
+export class WhoAmI {}
